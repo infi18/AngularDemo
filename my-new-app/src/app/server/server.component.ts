5bdedfb02822e16d07610e93637dd6616e1d4d04
infi18 <siddhi.naik18@gmail.com>
2021-06-02 16:32:20 -0500

    Added String Interpolation and Property Binding demo

--- my-new-app/src/app/server/server.component.ts
+++ my-new-app/src/app/server/server.component.ts
@@ -2,14 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
+  templateUrl: 'server.component.html',
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+
+  //ways to define a variable, type of variable can be mentioned but it is not mandatory to mention the type
+
+  serverID: number = 10;
+  serverStatus: string = 'online';
+  serverID1 = 20;
+  serverStatus1 = 'offline';
+
+  //Defining a method/function to fetch the value of Server
+  getServerStatus() {
+    return this.serverStatus1;  //fetches the value from variable server1 and returns that
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
--- my-new-app/src/app/servers/servers.component.ts
+++ my-new-app/src/app/servers/servers.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //component in need not be an element it can be any of the following
   selector: 'app-servers',
+  //selector: '[app-servers]',    //an attribute
+  //selector: '.app-servers',     //a class just like css
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  constructor() {
+
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
